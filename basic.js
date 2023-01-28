42ba10ce923277ddd1017efe1a7912cf7e9d6fac
emon3455 <emon.mhk69@gmail.com>
2023-01-28 23:26:24 +0600

    basic intro done

---
+++ basic.js
@@ -0,0 +1,18 @@
+
+//1. what is js ?
+/* 
+
+JavaScript is a programming language that allows you to implement complex functionalities on web pages.
+
+ */ 
+
+//2. how js works ? 
+/* 
+JS is a single threaded and single concurrent programming language which means it can handle one task at a time or, in other words, a piece of code at a time. It's an interpreted programming language.
+ */
+
+// data types in js:
+/* 
+1. Primitative
+2. Non Primitative
+*/
