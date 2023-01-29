b3abda6e476307d4bc23931922ba7536a34f9a2c
emon3455 <emon.mhk69@gmail.com>
2023-01-29 15:19:02 +0600

    variable done

---
+++ variable.js
@@ -0,0 +1,25 @@
+
+var num1 = 10;
+var num2 = 20;
+
+var sum = num1 + num2;
+var sub = num2 - num1;
+var mul = num1 * num2;
+var dev = num2 / num1;
+var rem = num2 % num1;
+console.log("sum: "+sum+"\nSubstract: "+sub+"\nMultiplications: "+mul+"\nDivison: "+dev+"\nRemainder: "+rem);
+
+var x = 10;
+var y = 3;
+
+var opr = x / y;
+console.log(opr);
+
+opr = opr.toFixed(2);
+console.log(opr);
+
+opr = parseFloat(opr);
+console.log(opr);
+
+opr = parseInt(opr);
+console.log(opr);
